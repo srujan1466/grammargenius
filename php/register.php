7ecd3c9356d5180d5161f8cb2f9c3621474e9afc
SRUJAN <srujan.14062006@gmail.com>
2025-05-02 01:07:48 +0530

    register all data

--- php/register.php
+++ php/register.php
@@ -6,29 +6,31 @@ include ("db.php");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = trim(string: $_POST["username"]);
     $password = $_POST["password"];
+    $email=$_POST["email"];
+    $fullname=$P_POST["fullname"];;
 
 
     // Step 3: Check if user already exists
-    $stmt = $conn->prepare(query: "SELECT id,username FROM users WHERE username = ?");
+    $stmt = $conn->prepare(query: "SELECT id FROM users WHERE username = ?");
     $stmt->bind_param("s", $username);
     $stmt->execute();
     $stmt->store_result();
     if ($stmt->num_rows === 1) {
-        die("Username already taken. Try something cooler.");
+        die("Username already taken. Try something else.");
     }
 
     // Step 4: Hash the password
     $hashed_password =password_hash(password: $password, algo:PASSWORD_DEFAULT);
 
     // Step 5: Insert new user
-    $stmt = $conn->prepare(query: "INSERT INTO users (username, password) VALUES (?, ?)");
-    $stmt->bind_param( "ss", $username,  $hashed_password);
+    $stmt = $conn->prepare(query: "INSERT INTO users (username, password,fullname,email) VALUES (?, ?,?,?)");
+    $stmt->bind_param( "ssss", $username,  $hashed_password,$fullname,$email);
 
     if ($stmt->execute()) {
         header(header: "Location: ../topics/login.html");
         //echo "'Registration successful'. <a href='../topics/login.html'>Log in now</a>.";
     } else {
-        echo "Something went wrong. Try again later.";
+        echo "Something went wrong. Try again later.<a href='../topics/register.html'>Register again</a>";
     }
 
     $stmt->close();
