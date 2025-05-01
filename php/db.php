61719938e80af3c33d83b7c9aafa91d081ea8cdb
SRUJAN <srujan.14062006@gmail.com>
2025-05-02 00:48:05 +0530

    2 logins added

--- php/db.php
+++ php/db.php
@@ -4,6 +4,11 @@ $username = "root"; // Default username for XAMPP/MAMP
 $password = ""; // Default password for XAMPP/MAMP
 $dbname = "grammar";
 
+/*$servername = "sql301.infinityfree.com";
+$username = "if0_38272767"; // Default username for XAMPP/MAMP
+$password = "ftpproject123"; // Default password for XAMPP/MAMP
+$dbname = "if0_38272767_grammargenius";*/
+
 // Create connection
 $conn =new mysqli($servername, $username, $password, $dbname);
 
